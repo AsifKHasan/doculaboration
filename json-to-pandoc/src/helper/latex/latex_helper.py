bb267321eb7d2e4305a030a8fee8f408c0d472ca
Asif Hasan <asif.hasan@gmail.com>
2021-09-28 19:48:43 +0600

    Latex Section implementation

--- json-to-pandoc/src/helper/latex/latex_helper.py
+++ json-to-pandoc/src/helper/latex/latex_helper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 from helper.latex.latex_util import *
 
 #   ----------------------------------------------------------------------------------------------------------------
@@ -22,12 +23,16 @@ class Cell(object):
             self.effective_format = CellFormat(value.get('effectiveFormat'))
             for text_format_run in value.get('textFormatRuns', []):
                 self.text_format_runs.append(TextFormatRun(text_format_run))
+
+            self.note = CellNote(value.get('note'))
+
         else:
             self.user_entered_value = None
             self.effective_value = None
             self.formatted_value = None
             self.user_entered_format = None
             self.effective_format = None
+            self.note = None
 
 
 ''' gsheet Row object wrapper
@@ -41,6 +46,21 @@ class Row(object):
         for value in row_data.get('values', []):
             self.cells.append(Cell(value))
 
+    def is_empty(self):
+        return (len(self.cells) == 0)
+
+    def get_cell(self, c):
+        if c >= 0 and c < len(self.cells):
+            return self.cells[c]
+        else:
+            return None
+
+    def is_out_of_table(self):
+        if len(self.cells) > 0:
+            # the first cell is teh relevant cell only
+            return self.cells[0].note.out_of_table
+        else:
+            return False
 
 ''' gsheet rowMetaData object wrapper
 '''
@@ -231,6 +251,43 @@ class TextFormatRun(object):
             self.format = None
 
 
+''' gsheet cell notes object wrapper
+'''
+class CellNote(object):
+
+    ''' constructor
+    '''
+    def __init__(self, note_json=None):
+        self.style = None
+        self.out_of_table = False
+        self.table_spacing = True
+        self.header_rows = 0
+        self.new_page = False
+        self.keep_with_next = False
+        self.page_number_style = None
+
+        if note_json:
+            try:
+                note_dict = json.loads(note_json)
+            except json.JSONDecodeError:
+                note_dict = {}
+
+            self.style = note_dict.get('style')
+
+            content = note_dict.get('content')
+            if content is not None and content == 'out-of-cell':
+                self.out_of_table = True
+
+            spacing = note_dict.get('table-spacing')
+            if spacing is not None and spacing == 'no-spacing':
+                self.table_spacing = False
+
+            self.header_rows = int(note_dict.get('header-rows', 0))
+            self.new_page = note_dict.get('new-page') is not None
+            self.keep_with_next = note_dict.get('keep-with-next') is not None
+            self.page_number_style = note_dict.get('page-number')
+
+
 ''' gsheet vertical alignment object wrapper
 '''
 class VerticalAlignment(object):
@@ -280,6 +337,7 @@ class LatexSection(object):
 
         section_contents = section_data.get('contents')
 
+        self.title = None
         self.row_count = 0
         self.column_count = 0
 
@@ -308,9 +366,11 @@ class LatexSection(object):
             sheets = section_contents.get('sheets')
             if isinstance(sheets, list) and len(sheets) > 0:
                 sheet_properties = sheets[0].get('properties')
-                if sheet_properties and 'gridProperties' in sheet_properties:
-                    self.row_count = max(int(sheet_properties['gridProperties'].get('rowCount', 0)) - 2, 0)
-                    self.column_count = max(int(sheet_properties['gridProperties'].get('columnCount', 0)) - 1, 0)
+                if sheet_properties:
+                    self.title = sheet_properties.get('title')
+                    if 'gridProperties' in sheet_properties:
+                        self.row_count = max(int(sheet_properties['gridProperties'].get('rowCount', 0)) - 2, 0)
+                        self.column_count = max(int(sheet_properties['gridProperties'].get('columnCount', 0)) - 1, 0)
 
                 data_list = sheets[0].get('data')
                 if isinstance(data_list, list) and len(data_list) > 0:
@@ -337,11 +397,83 @@ class LatexSection(object):
         else:
             self.has_content = False
 
-    def to_latex(self):
-        # start the section
-        header_lines = []
+        # we need a list to hold the tables and block for the cells
+        self.content_list = []
+        self.process()
 
+
+    ''' processes the cells to generate the proper order of tables and blocks
+    '''
+    def process(self):
+        # header
+        header_block = LatexSectionHeader(self.section_spec, self.section_break, self.level, self.no_heading, self.section, self.heading, self.title)
+        self.content_list.append(header_block)
+
+        # we have a concept of in-cell content and out-of-cell content
+        # in-cell contents are treated as part of a table, while out-of-cell contents are treated as independent paragraphs, images etc. (blocks)
+        next_table_starts_in_row = 0
+        next_table_ends_in_row = 0
+        for r in range(0, self.row_count):
+            # the first cell of the row tells us whether it is in-cell or out-of-cell
+            data_row = self.cell_matrix[r]
+            if data_row.is_out_of_table() == True:
+                # there may be a pending/running table
+                if r > next_table_starts_in_row:
+                    table = LatexTable(self.cell_matrix, next_table_starts_in_row, r - 1)
+                    self.content_list.append(table)
+
+                block = LatexParagraph(data_row, r)
+                self.content_list.append(block)
+
+                next_table_starts_in_row = r + 1
+
+            else:
+                next_table_ends_in_row = r
+
+        # there may be a pending/running table
+        if next_table_ends_in_row >= next_table_starts_in_row:
+            table = LatexTable(self.cell_matrix, next_table_starts_in_row, next_table_ends_in_row)
+            self.content_list.append(table)
+
+
+    ''' generates the latex code
+    '''
+    def to_latex(self):
+        latex_lines = []
+
+        # iterate to through tables and blocks contents
+        for block in self.content_list:
+            latex_lines = latex_lines + block.to_latex()
+
+        return '\n'.join(latex_lines)
+
+
+''' Latex Block object wrapper base class (plain latex, table, header etc.)
+'''
+class LatexBlock(object):
+
+    ''' generates latex code
+    '''
+    def to_latex(self):
+        pass
+
+
+''' Latex Header Block wrapper
+'''
+class LatexSectionHeader(LatexBlock):
+
+    ''' constructor
+    '''
+    def __init__(self, section_spec, section_break, level, no_heading, section, heading, title):
+        self.section_spec, self.section_break, self.level, self.no_heading, self.section, self.heading, self.title = section_spec, section_break, level, no_heading, section, heading, title
+
+    ''' generates latex code
+    '''
+    def to_latex(self):
+        header_lines = []
+        header_lines.append('')
         header_lines.append(begin_latex())
+        header_lines.append(f"% LatexSection: {self.title}")
 
         if self.section_break.startswith('newpage_'):
             header_lines.append(f"\\newpage")
@@ -362,24 +494,57 @@ class LatexSection(object):
             # headings are styles based on level
             if self.level != 0:
                 header_lines.append(heading_text)
-                header_lines.append('\n\n')
+                header_lines.append('\n')
             else:
                 header_lines.append(begin_latex())
                 header_lines.append(f"\\titlestyle{{{heading_text}}}")
                 header_lines.append(end_latex())
 
-        header_latex = '\n'.join(header_lines)
-
-        latex = header_latex
-
-        return latex
+        return header_lines
 
 
 ''' Latex Table object wrapper
 '''
-class LatexTable(object):
+class LatexTable(LatexBlock):
 
     ''' constructor
     '''
-    def __init__(self, row_data_list):
-        pass
+    def __init__(self, cell_matrix, start_row, end_row):
+        self.start_row, self.end_row = start_row, end_row
+        self.table_cell_matrix = cell_matrix[start_row:end_row+1]
+        self.row_count = len(self.table_cell_matrix)
+
+    ''' generates the latex code
+    '''
+    def to_latex(self):
+        table_lines = []
+        table_lines.append(begin_latex())
+        table_lines.append(f"% LatexTable: ({self.start_row}-{self.end_row}) : {self.row_count} rows")
+
+        # TODO: generate the table
+
+        table_lines.append(end_latex())
+        return table_lines
+
+
+''' Latex Block object wrapper
+'''
+class LatexParagraph(LatexBlock):
+
+    ''' constructor
+    '''
+    def __init__(self, data_row, row_number):
+        self.data_row = data_row
+        self.row_number = row_number
+
+    ''' generates the latex code
+    '''
+    def to_latex(self):
+        block_lines = []
+        block_lines.append(begin_latex())
+        block_lines.append(f"% LatexParagraph: row {self.row_number}")
+
+        # TODO: generate the block
+
+        block_lines.append(end_latex())
+        return block_lines
