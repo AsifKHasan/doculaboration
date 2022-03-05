e0b9fe5e0ba2eb19a7bb0b68b7c3232be2336685
Asif Hasan <asif.hasan@gmail.com>
2022-03-05 19:55:42 +0600

    pdf-from-gsheet bash script

---
+++ pdf-from-gsheet.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# gsheet->json->pandoc->pdf pipeline
+
+set echo off
+
+# parameters
+DOCUMENT="MoL-DLRMS__eoi-and-tor"
+DOCULABORATION_BASE="/home/asif/projects/asif@github/doculaboration"
+
+cd ${DOCULABORATION_BASE}
+
+# json-from-gsheet
+pushd ./gsheet-to-json/src
+./json-from-gsheet.py --config "../conf/config.yml" --gsheet ${DOCUMENT}
+
+if [ ${?} -ne 0 ]; then
+  popd && exit 1
+else
+  popd
+fi
+
+# pandoc-from-json
+pushd ./json-to-pandoc/src
+./pandoc-from-json.py --config "../conf/config.yml" --json ${DOCUMENT}
+
+if [ ${?} -ne 0 ]; then
+  popd && exit 1
+else
+  popd
+fi
+
+# pandoc-from-json
+pushd ./out
+time pandoc ${DOCUMENT}.mkd ../json-to-pandoc/conf/preamble.yml -s --pdf-engine=lualatex -f markdown -t latex -o ${DOCUMENT}.pdf
+
+if [ ${?} -ne 0 ];  then
+  popd && exit 1
+else
+  popd
+fi
