2d5e0948bb70771b02eaab9cd9773e80820391d6
Asif Hasan <asif.hasan@gmail.com>
2023-02-27 01:21:03 +0600

    updated

--- json-to-docx/src/doc/doc_api.py
+++ json-to-docx/src/doc/doc_api.py
@@ -46,7 +46,7 @@ class DocxSectionBase(object):
         self.margin_spec_name = self.section_prop['margin-spec']
         self.margin_spec = self._config['page-specs']['margin-spec'][self.margin_spec_name]
 
-        self.background_image = self.section_prop['background_image']
+        self.background_image = self.section_prop['background-image']
 
         self.document_index = self.section_meta['document-index']
         self.document_name = self.section_meta['document-name']
