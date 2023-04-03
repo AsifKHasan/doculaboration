8f9472be5ca2e328c0904fc73fcdaa314bfff759
Asif Hasan <asif.hasan@gmail.com>
2023-04-03 21:13:02 +0600

    updated

--- docx-from-gsheet.bat
+++ docx-from-gsheet.bat
@@ -7,7 +7,7 @@ set DOCUMENT=%1
 
 :: json-from-gsheet
 pushd .\gsheet-to-json\src
-.\json-from-gsheet.py --config "../conf/config.yml" --gsheet "%DOCUMENT%"
+python json-from-gsheet.py --config "../conf/config.yml" --gsheet "%DOCUMENT%"
 
 if errorlevel 1 (
   popd
@@ -18,7 +18,7 @@ popd
 
 :: docx-from-json
 pushd .\json-to-docx\src
-.\docx-from-json.py --config "../conf/config.yml" --json "%DOCUMENT%"
+python docx-from-json.py --config "../conf/config.yml" --json "%DOCUMENT%"
 
 if errorlevel 1 (
   popd
--- odt-from-gsheet.bat
+++ odt-from-gsheet.bat
@@ -7,7 +7,7 @@ set DOCUMENT=%1
 
 :: json-from-gsheet
 pushd .\gsheet-to-json\src
-.\json-from-gsheet.py --config "../conf/config.yml" --gsheet "%DOCUMENT%"
+python json-from-gsheet.py --config "../conf/config.yml" --gsheet "%DOCUMENT%"
 
 if errorlevel 1 (
   popd
@@ -18,7 +18,7 @@ popd
 
 :: odt-from-json
 pushd .\json-to-odt\src
-.\odt-from-json.py --config "../conf/config.yml" --json "%DOCUMENT%"
+python odt-from-json.py --config "../conf/config.yml" --json "%DOCUMENT%"
 
 if errorlevel 1 (
   popd
