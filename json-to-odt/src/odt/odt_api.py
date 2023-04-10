e40481ee7426b9a38f086be1a8346f1e46428fe1
Asif Hasan <asif.hasan@gmail.com>
2023-04-10 21:12:24 +0600

    updated

--- json-to-odt/src/odt/odt_api.py
+++ json-to-odt/src/odt/odt_api.py
@@ -65,7 +65,7 @@ class OdtSectionBase(object):
         self.page_layout = get_page_layout(self._odt, self.page_layout_name)
 
         # if it is the very first section, change the page-layout of the *Standard* master-page
-        if self.first_section:
+        if self.first_section and self.document_index == 0:
             self.master_page_name = 'Standard'
             update_master_page_page_layout(self._odt, master_page_name='Standard', new_page_layout_name=self.page_layout_name)
 
