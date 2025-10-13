54338ec4525fdc1a8d49316f1fedb1af82691047
Asif Hasan <asif.hasan@gmail.com>
2025-10-13 21:38:29 +0600

    updated

--- json-to-docx/src/doc/doc_helper.py
+++ json-to-docx/src/doc/doc_helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 ''' MS Office docx wrapper objects
 '''
--- json-to-odt/src/odt/odt_helper.py
+++ json-to-odt/src/odt/odt_helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 ''' Openoffice odt wrapper objects
 '''
